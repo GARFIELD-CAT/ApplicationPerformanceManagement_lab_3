95aea2a44fc86f65b30bc6c1eed32f9ff5c3cbcd
Yagunov Denis <spb.jagunov.denis@gmail.com>
2026-02-07 19:29:08 +0300

    init project

---
+++ src/main/java/ru/dayagunov/lab_3/Lab3Application.java
@@ -0,0 +1,14 @@
+package ru.dayagunov.lab_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3Application.class, args);
+        System.out.println("OK");
+	}
+
+}
